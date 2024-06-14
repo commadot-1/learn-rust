34407cde79f720950391c7541320a755e80cdcb1
Bastani Saaz <bastanisaaz@Bastanis-MacBook-Pro.local>
2024-06-13 23:08:43 -0400

    chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
-slint::include_modules!();
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui = AppWindow::new()?;
 
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
+fn main() {
+    println!("Guess the number!");
+    //let ui = AppWindow::new()?;
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+   
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
-    });
-
-    ui.run()
+      
+    }
+   
 }
